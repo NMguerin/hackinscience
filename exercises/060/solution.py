4c446e290dd23d19c00e69d848a11319d0864513
NMguerin <nina.guerin@cri-paris.org>
2015-11-06 16:55:56 +0100

    sauvegarde

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -1,5 +1,5 @@
 import string
 alpha = string.ascii_lowercase
 for i in range(0, len(alpha)):
-     for j in range(0, len(alpha)):
-         print("%s%s" % (alpha[i], alpha[j]))
+    for j in range(0, len(alpha)):
+        print("%s%s" % (alpha[i], alpha[j]))
